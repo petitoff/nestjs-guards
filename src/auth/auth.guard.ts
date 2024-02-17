a1d98d8869a3e32abaac9e1c3fb7a29c56313bc5
Błażej Domagała <petitoff@outlook.com>
2024-02-17 21:23:15 +0100

    Add dotenv package and use environment variable for JWT secret

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -20,7 +20,7 @@ export class AuthGuard implements CanActivate {
     }
     try {
       const payload = await this.jwtService.verifyAsync(token, {
-        secret: 'YOUR_SECRET_KEY',
+        secret: process.env.JWT_SECRET,
       });
       // 💡 We're assigning the payload to the request object here
       // so that we can access it in our route handlers
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,11 +4,11 @@ import * as jwt from 'jsonwebtoken';
 
 @Injectable()
 export class AuthService {
-  private readonly jwtSecret = 'YOUR_SECRET_KEY'; // Replace with your secret key
+  private readonly jwtSecret = process.env.JWT_SECRET; // Replace with your secret key
 
   login(loginDto: LoginDto) {
     const payload = { username: loginDto.username, sub: loginDto.userId };
-    const token = jwt.sign(payload, this.jwtSecret);
+    const token = jwt.sign(payload, this.jwtSecret, { expiresIn: '48h' });
     return { access_token: token };
   }
 }
