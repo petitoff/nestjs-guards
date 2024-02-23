d504876f0777f61a08dc56f201d34f6229958eba
Błażej Domagała <petitoff@outlook.com>
2024-02-23 16:43:30 +0100

    Add error handling and logging to UsersController

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,20 +1,32 @@
-import { Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Controller, Get, Logger, Post, UseGuards } from '@nestjs/common';
 import { UsersGuard } from 'src/users/users.guard';
 import { Roles } from 'src/roles/roles.decorator';
 import { RolesGuard } from 'src/roles/roles.guard';
 
 @Controller('users')
 export class UsersController {
+  private logger = new Logger(UsersController.name);
+
   @Get()
   @UseGuards(UsersGuard)
   getUsers(): { message: string } {
-    return { message: 'This action returns all users' };
+    try {
+      return { message: 'This action returns all users' };
+    } catch (error) {
+      this.logger.error(error.message);
+      throw error;
+    }
   }
 
   @Post()
   @UseGuards(RolesGuard)
   @Roles(['admin'])
   postUsers(): { message: string } {
-    return { message: 'This action adds a new user' };
+    try {
+      return { message: 'This action adds a new user' };
+    } catch (error) {
+      this.logger.error(error.message);
+      throw error;
+    }
   }
 }
